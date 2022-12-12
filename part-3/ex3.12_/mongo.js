04a9b05d979ba71e46d69ce4f92b79b8fd17641f
Koushik Dey <koushik.dey@live.com>
2022-12-12 21:20:06 +0200

    :white_check_mark: part3_3.12-70% done
    done

---
+++ part-3/ex3.12_/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://finfotek80:${password}@finfotek.elgkz.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  id: String,
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url)
+  .then((result) => {
+    console.log('connected')
+    const person = new Person({
+      id: '10',
+      name: 'Lari',
+      number: '040-7403490',
+    })
+    return person.save()
+  }
+);
+Person.find({})
+  .then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+  .catch((err) => console.log(err))
